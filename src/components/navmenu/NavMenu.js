51940614f493e6bddb91a8be59ed24e7849d1fed
James Liu <james_liu@fastmail.com>
2017-07-29 20:55:10 +1000

    Squashed commit of the following:
    commit 26419479f5abe4fb9da9d61106a9a6aff1d0df84
    Author: James Liu <james_liu@fastmail.com>
    Date:   Sat Jul 29 20:52:55 2017 +1000
    
        Squashed commit of the following:
    
        commit 7149fbf3ffd8719b83fe23db77b3f2a6e19ebdaf
        Author: James Liu <james_liu@fastmail.com>
        Date:   Sat Jul 29 20:51:51 2017 +1000
    
            removed text decoration from nav links
    
        commit bc530eaa0d363e6787d0d9e87ca4451b503dafe9
        Author: James Liu <james_liu@fastmail.com>
        Date:   Sat Jul 29 20:42:18 2017 +1000
    
            cleaned up Nav and NavMenu code

--- src/components/navmenu/NavMenu.js
+++ src/components/navmenu/NavMenu.js
@@ -10,106 +10,79 @@ import iconFamilyHealth from '../../images/icons/familyhealth/family-health.png'
 import iconClinics from '../../images/icons/extraicons/findahealthservice.png';
 
 class NavMenu extends Component {
-  state = {
-    menu: true,
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      menu: true,
+      menuItems: [
+        {
+          icon: iconAccess,
+          name: 'Access',
+          url: '/access'
+        },
+        {
+          icon: iconFamilyPlanning,
+          name: 'Family Planning',
+          url: '/family-planning'
+        },
+        {
+          icon: iconPregnancy,
+          name: 'Pregnancy and Newborn',
+          url: '/pregnancy-newborn'
+        },
+        {
+          icon: iconSexualHealth,
+          name: 'Sexual Health',
+          url: '/sexual-health'
+        },
+        {
+          icon: iconFamilyHealth,
+          name: 'Family and Social Health',
+          url: '/family-health'
+        },
+        {
+          icon: iconClinics,
+          name: 'Clinic Locations',
+          url: '/clinics'
+        },
+      ]
+    }
+    this.showMenu = this.showMenu.bind(this)
+  }
 
   showMenu() {
     this.setState({menu: !this.state.menu});
-  };
+  }
+
   render() {
     return (
       <div>
         <button onClick={() => this.showMenu()} className="toggleMenu">{this.state.menu ? '-' : '+'}</button>
-
         <div className="menu">
-          <Link to="/access">
-            <div className="item item-1">
-              <img className="icon" src={iconAccess} alt="Access"/>
-              <div className="item-text"><p>{this.props.arabic ? "Access" : "تسجيل الدخول"}</p></div>
-            </div>
-          </Link>
-
-          <Link to="/family-planning">
-            <div className="item item-2">
-              <img className="icon" src={iconFamilyPlanning} alt="Family Planning"/>
-              <div className="item-text"><p>{this.props.arabic ? "Family Planning" : "نظيم الأسرة"}</p></div>
-            </div>
-          </Link>
-
-          <Link to="/pregnancy-newborn">
-            <div className="item item-3">
-              <img className="icon" src={iconPregnancy} alt="Pregnancy and Newborn"/>
-              <div className="item-text"><p>{this.props.arabic ? "Pregnancy Newborn" : "الحمل وحديثي الولادة"}</p></div>
-            </div>
-          </Link>
-
-          <Link to="/sexual-health">
-            <div className="item item-4">
-              <img className="icon" src={iconSexualHealth} alt="Sexual Health"/>
-              <div className="item-text"><p>{this.props.arabic ? "Sexual Health" : "الحياة الجنسية"}</p></div>
-            </div>
-          </Link>
-
-          <Link to="/family-health">
-            <div className="item item-5">
-              <img className="icon" src={iconFamilyHealth} alt="Family and Social Health"/>
-              <div className="item-text"><p>{this.props.arabic ? "Family Health" : "صحة المجتمع والعائلة"}</p></div>
-            </div>
-          </Link>
-
-          <Link to="/clinics">
-            <div className="item item-6">
-              <img className="icon" src={iconClinics} alt="Clinics Location"/>
-              <div className="item-text"><p>{this.props.arabic ? "Clinic Locations" : "Clinic arabic"}</p></div>
-            </div>
-          </Link>               
+          {this.state.menuItems.map((item, index) => {
+            return (
+              <Link to={item.url}>
+                <div className={`item item-${index + 1}`}>
+                  <img className="icon" src={item.icon} alt="Access"/>
+                  <div className="item-text"><p>{item.name}</p></div>
+                </div>
+              </Link>
+            )
+          })}            
         </div>
 
-
         {this.state.menu && 
-        <div className="mobile-menu">
-          <Link to="/access">
-            <div className="mobile-item item-1">
-              <img className="icon" src={iconAccess} alt="Access"/>
-              <div>{this.props.arabic ? "Access" : "تسجيل الدخول"}</div>
-            </div>
-          </Link>
-
-          <Link to="/family-planning">
-            <div className="mobile-item item-2">
-              <img className="icon" src={iconFamilyPlanning} alt="Family Planning"/>
-              <div>{this.props.arabic ? "Family Planning" : "نظيم الأسرة"}</div>
-            </div>
-          </Link>
-
-          <Link to="/pregnancy-newborn">
-            <div className="mobile-item item-3">
-              <img className="icon" src={iconPregnancy} alt="Pregnancy and Newborn"/>
-              <div>{this.props.arabic ? "Pregnancy Newborn" : "الحمل وحديثي الولادة"}</div>
-            </div>
-          </Link>
-
-          <Link to="/sexual-health">
-            <div className="mobile-item item-4">
-              <img className="icon" src={iconSexualHealth} alt="Sexual Health"/>
-              <div>{this.props.arabic ? "Sexual Health" : "الحياة الجنسية"}</div>
-            </div>
-          </Link>
-
-          <Link to="/family-health">
-            <div className="mobile-item item-5">
-              <img className="icon" src={iconFamilyHealth} alt="Family and Social Health"/>
-              <div>{this.props.arabic ? "Family Health" : "صحة المجتمع والعائلة"}</div>
-            </div>
-          </Link>
-
-          <Link to="/clinics">
-            <div className="mobile-item item-6">
-              <img className="icon" src={iconClinics} alt="Clinics Location"/>
-              <div>{this.props.arabic ? "Clinic Locations" : "Clinic arabic"}</div>
-            </div>
-          </Link>               
+          <div className="mobile-menu">
+             {this.state.menuItems.map((item, index) => {
+              return (
+                <Link to={item.url}>
+                  <div className={`mobile-item item-${index + 1}`}>
+                    <img className="icon" src={item.icon} alt="Access"/>
+                    <div className="item-text"><p>{item.name}</p></div>
+                  </div>
+                </Link>
+              )
+            })} 
         </div>}
       </div>
     )
